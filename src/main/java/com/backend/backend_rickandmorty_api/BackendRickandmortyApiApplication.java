a1fd00ff6d6670a9f9f7e830a6a95fcc5904216b
Azul-20 <rael.rivero01@gmail.com>
2025-06-07 17:54:52 -0500

    creación del backend

---
+++ src/main/java/com/backend/backend_rickandmorty_api/BackendRickandmortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backend_rickandmorty_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRickandmortyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRickandmortyApiApplication.class, args);
+	}
+
+}
